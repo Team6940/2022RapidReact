16cb232f140b3ab4d8b52eb94af46b1f0872a4a8
mendax1234 <daniel.wb.zhu@outlook.com>
2022-08-18 19:15:30 +0800

    Clean

--- src/main/java/frc/robot/auto/AutonomousSelector.java
+++ src/main/java/frc/robot/auto/AutonomousSelector.java
@@ -18,12 +18,9 @@ public class AutonomousSelector {
         ShuffleboardTab autoTab = Shuffleboard.getTab("Auto settings");
 
         autonomousModeChooser = new SendableChooser<>();
-        autonomousModeChooser.setDefaultOption("FiveBallBottom2", AutonomousMode.FIVE_BALL_BOTTOM2);
-        autonomousModeChooser.addOption("SixBall2", AutonomousMode.SIX_BALL2);
-        autonomousModeChooser.addOption("TwoBall2", AutonomousMode.TWO_BALL2);
+        autonomousModeChooser.setDefaultOption("FiveBallBottom", AutonomousMode.FIVE_BALL_BOTTOM);
         autonomousModeChooser.addOption("SixBall", AutonomousMode.SIX_BALL);
         autonomousModeChooser.addOption("TwoBall", AutonomousMode.TWO_BALL);
-        autonomousModeChooser.addOption("FiveBallBottom", AutonomousMode.FIVE_BALL_BOTTOM);
 
         autoTab.add("autoMode", autonomousModeChooser);
     }
@@ -39,16 +36,7 @@ public class AutonomousSelector {
                 return new SixBallMode(s_Swerve);
 
             case TWO_BALL:
-                return new TwoBallMode(s_Swerve);
-
-            case FIVE_BALL_BOTTOM2:
-                return new FiveBallBottomMode(s_Swerve);      
-
-            case SIX_BALL2:
-                return new SixBallMode(s_Swerve);
-
-            case TWO_BALL2:
-                return new TwoBallMode(s_Swerve);                
+                return new TwoBallMode(s_Swerve);             
 
             default:
                 System.out.println("ERROR: unexpected auto mode: " + mode);
@@ -64,10 +52,7 @@ public class AutonomousSelector {
     private enum AutonomousMode {
         FIVE_BALL_BOTTOM,
         SIX_BALL,
-        TWO_BALL,
-        FIVE_BALL_BOTTOM2,
-        SIX_BALL2,
-        TWO_BALL2,        
+        TWO_BALL   
     }
 
     public static Pose2d getStartingPose(){
